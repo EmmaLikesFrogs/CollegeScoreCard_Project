513ee634cb3c63d26f732d66c60c542bf17ddda2
Jin YuHan <jin.burgess19@ncf.edu>
2023-10-05 16:36:48 -0400

    Update 2

--- Import_files.R
+++ Import_files.R
@@ -11,50 +11,40 @@ raw_files <- data.frame(
   filename = list.files(
   'CollegeScorecard_Raw_Data_08032021'))
 
-# creates a table with the file names and the path associated with it 
+# creates a table with the file names and the path associated with it
 raw_file_paths <- raw_files  %>%
   mutate(filepath = paste0(
     'CollegeScorecard_Raw_Data_08032021/', filename))
-
 
 # Depending on how many files we want to look at will determine which ones we combine togeth
 # Some of the files do not have the same stricture time for each column (one column could be a double but then
 # a character in the next file).
 
-# combine_csv_files rbinds the files together even with these discrepencies 
+# combine_csv_files rbinds the files together even with these discrepancies
 combine_csv_files <- function(raw_file_paths) {
   # Initialize an empty list to store data frames
   df_list <- list()
-  
+
   # Loop through each CSV file and read it into a data frame
   for (file_path in raw_file_paths[,2]) {
     #stringAsFactors doesn't assign strings to a facotr/ value (setting string to factor is more efficient)
     # may run a little less efficient but because of size of data, it should be okay
-    df <- read.csv(file_path, stringsAsFactors = FALSE) 
+    df <- read.csv(file_path, stringsAsFactors = FALSE)
     df_list <- append(df_list, list(df))
   }
-  
+
   # Combine the data frames using rbindlist from the data.table package
   combined_df <- data.table::rbindlist(df_list, fill = TRUE)
   return(combined_df)
 }
 
 
-combined_df <- combine_csv_files(raw_file_paths)
+combined_data <- combine_csv_files(raw_file_paths)
 
+names <-apply(combined_df, 2, function(x) sum(x !='NULL')/length(x) > 0.75)
 
-# determining completeness of each column to figure out which ones may be best to start looking at 
-total_col_entry <- nrow(combined_df)
-completeness <- c()
-for (i in 1:ncol(combined_df)){
-  freq_NULL <- table(combined_df[,..i])
-  count_of_null <- (freq_NULL['NULL']/total_col_entry)*100
-  completeness <- append(completeness, count_of_null)
-}
-total_null_feq <- data.frame(colnames(combined_df),completeness)%>%
-  # renaming the columns so it makes more sense (new name, old name)
-  rename('column' = 'colnames.combined_df.',
-         'Null freq' = 'completeness')
+test1<- as.data.frame(combined_df)
+trimmed_combined_df<-combined_df[, c(names)]
 
-# determining threshold for which columns we may want to look at based of completeness of each column
-subset_null_feq <-subset(x, x$`Null freq` <50)
+write.csv(trimmed_combined_df,
+          "/Users/yuhanburgess/Documents/GitHub/DataMungingProject2/trimmed_combined_df.csv", row.names=TRUE)
---
+++ further_trimming.R
@@ -0,0 +1,27 @@
+test1 <- subset(test1, test1$ICLEVEL != 'Null')
+
+# PCIP11: Percentage of degrees awarded in Computer & Information Sciences and Support Services
+# PCIP14: Percentage of degrees awarded in Engineering.
+# PCIP15: Percentage of degrees awarded in Engineering Technologies & Engineering-Related Fields.
+# PCIP27: Percentage of degrees awarded in Mathematics & Statistics.
+# PCIP41: Percentage of degrees awarded in Science Technologies/Technicians.
+
+# CIP11CERT1  CIP11CERT2  CIP11CERT4  CIP11BACHL
+# CIP11CERT1: Cert. <1 academic yr in Computer & Information Sciences & Support Services.
+# CIP11CERT2: Cert. >1<2 academicyrs in Computer & Information Sciences & Support Services.
+# CIP11ASSOC: Associate degree in Computer And Information Sciences And Support Services.
+# CIP11CERT4: Award >2<4 academic yrs in Computer & Information Sciences & Support Services.
+# CIP11BACHL: Bchlr's deg. in Computer & Information Sciences & Support Services.
+
+
+# CIP14CERT1  CIP14CERT2  CIP14ASSOC  CIP14CERT4  CIP14BACHL
+# same pattern as lines 16-20 but for engineering
+
+# CIP15CERT1  CIP15CERT2  CIP15ASSOC  CIP15CERT4  CIP15BACHL
+# same pattern as lines 16-20 but for Engineering Technologies & Engineering-Related Fields.
+
+# CIP27CERT1  CIP27CERT2  CIP27ASSOC  CIP27CERT4  CIP27BACHL
+# same pattern as lines 16-20 but for Mathematics & Statistics
+
+# CIP41CERT1  CIP41CERT2  CIP41ASSOC  CIP41CERT4  CIP41BACHL
+# same pattern as lines 16-20 but for Computer & Information Sciences & Support Services.
