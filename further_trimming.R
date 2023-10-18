13240ab23a3d834b3c335775dbbb3c2656095705
JinBurgess <142836951+JinBurgess@users.noreply.github.com>
2023-10-18 12:16:51 -0400

    further_trimming

--- further_trimming.R
+++ further_trimming.R
@@ -1,18 +1,113 @@
 library(dplyr)
 library(ggplot2)
-names <-apply(dataframe, 2, function(x) sum(x !='NULL')/length(x) > 0.75)
 
-# test1<- as.data.frame(combined_df)
-trimmed_combined_df<-dataframe[, c(names)]
 
-# filtering based of predominately undergrad schools
-test1 <- subset(trimmed_combined_df, trimmed_combined_df$PREDDEG == 3)
+# names <-apply(dataframe, 2, function(x) sum(x !='NULL')/length(x) > 0.75)
+# 
+# # test1<- as.data.frame(combined_df)
+# trimmed_combined_df<-dataframe[, c(names)]
+# 
+# # filtering based of predominately undergrad schools
+# test1 <- subset(trimmed_combined_df, trimmed_combined_df$PREDDEG == 3)
+# 
+# test2 <- trimmed_combined_df %>%
+#   filter(PREDDEG == 3)%>%
+#   filter(year > 1999)
 
-test2 <- trimmed_combined_df %>%
-  filter(PREDDEG == 3)%>%
-  filter(year > 1999)
+library(readr)
+df <- read_csv("Documents/GitHub/DataMungingProject2/CollegeScorecard_Raw_Data_08032021/MERGED2021_22_PP.csv")
 
-graphTest<- ggplot(test2, aes(x = ))
+df1 <- df %>%
+  mutate_all(~ifelse(. == "NULL", NA, .)) # changing all NULL into NA
+
+df2 <- df1[, 1:450] # trimming df to look at range of variables that will mostlikly be used for testing
+
+#####################################################
+# testing
+table(df$CCBASIC)
+table(df2$CCBASIC)
+hist(as.numeric(df2$CCBASIC))
+total_na_values <- sum(is.na(df2$CCBASIC)) 
+######################################################
+
+# REMOVING ROWS THAT ARE 90% INCOMPLETE 
+# Calculate the percentage of NA values for each row
+row_na_percent <- rowSums(is.na(df2)) / ncol(df2) * 100
+# Find rows with 90% or more NA values
+rows_with_90_percent_or_more_na <- which(row_na_percent >= 90)
+# Print the row numbers that meet the condition
+print(df2[rows_with_90_percent_or_more_na, 4])
+
+# return df with 90% incomplete data
+na_df <- df2 %>%
+  filter(row_number() %in% rows_with_90_percent_or_more_na)
+
+# returns df that have rows that are less than 90% incomplete
+filtered_df <- df2 %>%
+  filter(!(row_number() %in% rows_with_90_percent_or_more_na))
+
+##########################################################
+# BARPLOT OF THE NUMBER OF COLLEGES IN EACH STATE
+# look at requency of institutions that are open and those that 
+# are closed 
+
+# MAY NEED TO NORMALIZE THE DATA 
+state_count <- filtered_df %>%
+  count('STABBR')
+state_count$zscore <- scale(state_count$freq)
+
+state_count_plot <- state_count %>%
+  mutate(STABBR = fct_reorder(STABBR, zscore))%>%
+  ggplot(aes(x=STABBR, y = zscore, fill= zscore)) + 
+  geom_bar(stat = 'identity', alpha = 0.7, width = 0.5) + 
+  coord_flip()+
+  scale_fill_gradient(low = "#69b3a2", high = "#FF0000", name = "Frequency") +
+  xlab('')+
+  ggtitle('Total Institutions')+
+  theme_classic()
+
+
+open_instit <- filtered_df%>%
+  filter(CURROPER == 1)%>%
+  count('STABBR')
+open_instit$zscore <- scale(open_instit$freq)
+
+open_instit_plot <- open_instit %>%
+  mutate(STABBR = fct_reorder(STABBR, zscore))%>%
+  ggplot(aes(x=STABBR, y = zscore, fill= zscore)) + 
+  geom_bar(stat = 'identity', alpha = 0.7, width = 0.5) + 
+  coord_flip()+
+  scale_fill_gradient(low = "#69b3a2", high = "#FF0000", name = "Frequency") +
+  xlab('')+
+  ggtitle('Open Institutions')+
+  theme_classic()
+
+closed_instit <- filtered_df%>%
+  filter(CURROPER == 0)%>%
+  count('STABBR')
+closed_instit$zscore <- scale(closed_instit$freq)
+
+closed_instit_plot <- closed_instit %>%
+  mutate(STABBR = fct_reorder(STABBR, zscore))%>%
+  ggplot(aes(x=STABBR, y = zscore, fill= zscore)) + 
+  geom_bar(stat = 'identity', alpha = 0.7, width = 0.5) + 
+  coord_flip()+
+  scale_fill_gradient(low = "#69b3a2", high = "#FF0000", name = "Frequency") +
+  xlab('')+
+  ggtitle('Closed Institutions')+
+  theme_classic()
+
+library(gridExtra)
+
+state_count_grob<- ggplotGrob(state_count_plot)
+open_instit_grob<- ggplotGrob(open_instit_plot)
+closed_instit_grob<- ggplotGrob(closed_instit_plot)
+# Arrange the plots in a single window
+combined_plots <- grid.arrange(state_count_grob, open_instit_grob, closed_instit_grob, nrow = 1)
+
+# Display the combined plots
+print(combined_plots)
+
 # PCIP11: Percentage of degrees awarded in Computer & Information Sciences and Support Services
 # PCIP14: Percentage of degrees awarded in Engineering.
 # PCIP15: Percentage of degrees awarded in Engineering Technologies & Engineering-Related Fields.
