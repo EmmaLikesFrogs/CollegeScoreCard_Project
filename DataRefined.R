27e8d029d97df990fa7148d9211056b15d3449f9
JinBurgess <142836951+JinBurgess@users.noreply.github.com>
2023-10-20 23:25:27 -0400

    data_manipulation_visualization

--- DataRefined.R
+++ DataRefined.R
@@ -18,40 +18,31 @@ df <- read_csv("csv_files/MERGED2021_22_PP.csv")
 # INITIAL FILTERING
 
 df_filter <- function(df) {
+  # changing all NULL values into NA values
   df<-df %>%
     mutate_all(~ifelse(. == "NULL", NA, .))
   
   # vis_miss(df, warn_large_data = FALSE)
   
-  df_test <- df[1:6049,]
-  vis_miss(df_test, warn_large_data = FALSE)
+  df_test <- df[1:6049,] # removing bottom rows since they do not contain important info for analysis
+  # vis_miss(df_test, warn_large_data = FALSE) # plotting missing and present data in csv file
   
+  # remove columns that are not 70% complete
   names <-apply(df_test, 2, function(x) sum(!is.na(x))/length(x) > 0.70)
-  df1<- as.data.frame(df)
-  df1<-df1[, c(names)]
+  df<- as.data.frame(df)
+  df<-df[, c(names)]
+   
+  df <- df_categorical(df) # change three columns from numeric to categorical data
   
   # vis_miss(df1, warn_large_data = FALSE)
   
-  return(df1)
+  return(df)
   
 }
-# categorizing columns for analysis
+
+# categorizing columns Highest degree, Predominant degree, and 
+# Carnegie classification columns
 df_categorical <- function(df){
-  df <- df[, 1:450] 
-  #REMOVING ROWS THAT ARE 90% INCOMPLETE 
-  # Calculate the percentage of NA values for each row
-  row_na_percent <- rowSums(is.na(df)) / ncol(df) * 100
-  # Find rows with 90% or more NA values
-  rows_with_90_percent_or_more_na <- which(row_na_percent >= 90)
-  
-  # MAY REMOVE LATER
-  na_df <- df %>%
-    filter(row_number() %in% rows_with_90_percent_or_more_na)
-  
-  # returns df that have rows that are less than 90% incomplete
-  filtered_df <- df %>%
-    filter(!(row_number() %in% rows_with_90_percent_or_more_na))
-  
   pred_deg_awarded <- c('Not classified', 'Certificate', 
                         'Associate', 'Bachelor', 
                         'Graduate')
@@ -76,21 +67,46 @@ df_categorical <- function(df){
                       'Special Focus 4-Years: Other Related-Technology Related Schools','Special Focus 4-Years: Business and Management Schools',
                       'Special Focus 4-Years: Arts, Music, and Design School','Special Focus 4-Years: Law Schools',
                       'Special Focus 4-Years: Other Special Focus Institutions','Tribal Colleges')
-       
-
+  
+  
   # changes numeric values to categorical
   valid_indx <- which (df$CCBASIC != -2) # -2 is not associated to a numeric value (not changing the value)
   df$CCBASIC[valid_indx] <- cc_basic_score [as.numeric(df$CCBASIC[valid_indx])+1]
   df$HIGHDEG <- high_deg_awarded[as.numeric(df$HIGHDEG)+1]
   df$PREDDEG <- pred_deg_awarded[as.numeric(df$PREDDEG)+1]
   
- return(df) 
+  return(df)
+}
+
+# further trimming of dataframe to be used in some anaylsis
+df_shorten <- function(df){
+  df <- df[, 1:450] 
+  #REMOVING ROWS THAT ARE 90% INCOMPLETE 
+  # Calculate the percentage of NA values for each row
+  row_na_percent <- rowSums(is.na(df)) / ncol(df) * 100
+  # Find rows with 90% or more NA values
+  rows_with_90_percent_or_more_na <- which(row_na_percent >= 90)
+  
+  # MAY REMOVE LATER
+  na_df <- df %>%
+    filter(row_number() %in% rows_with_90_percent_or_more_na)
+  
+  # returns df that have rows that are less than 90% incomplete
+  filtered_df <- df %>%
+    filter(!(row_number() %in% rows_with_90_percent_or_more_na))
+  
+  
+ return(filtered_df) 
 
 }
 
 # function filters out data that does not 
 # contain actual states 
+
+# this may not be needed since there is a built in state.abb that we use later on
+# but I am too pressed for time right now to change it
 state_isolation <- function (df){
+  # list of only states 
   states <- c('AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 
               'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 
               'IL', 'IN', 'IA', 'KS', 'KY', 'LA',
@@ -101,6 +117,7 @@ state_isolation <- function (df){
               'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 
               'WI', 'WY')
   
+  # remove any territories of the US that are not considered states 
   filtered_df <- df%>%
     filter(STABBR %in% states)
   
@@ -133,38 +150,193 @@ group_bar_filter <- function(df){
   # retrieving two columns 
   state_class <- filtered_df[c('STABBR', 'PREDDEG')]
 
-  # count occurenaces based on the degree and which state it is from
+  # count occurrences based on the degree and which state it is from
   unique_combo <- state_class%>%
     group_by(STABBR, state_class[2])%>%
     summarise(UniqueCount = n())
   
   # check to make sure we are only looking at states
   unique_combo <- state_isolation(unique_combo)
+  
   # create a new column of z-scores just in case
+  # never actually used but oh well (just using up memory)
   unique_combo$zscore <- scale(unique_combo$UniqueCount)[,1] # normalizing calculations using z-score
   
   return(unique_combo)
 }
 
 # function filters data set to be used in 
-# density_plot function
+# ridge_plot function
 ridge_plot_filter <- function(df){
   demograph <-df[,264:273]  # retrieving information about student demographic
   pred_undergrad <- cbind(STABBR = df [,6], df[,14:15], CCBASIC = df[,22])# retrieving principal information about institution
   
   # this analysis looks specifically at special focus 4-year institutions and those that are considered baccalaureate
+  # when I wrote bach_stud I thought (Bach is a stud)
   bach_stud <- cbind(pred_undergrad,demograph)
   return(bach_stud)
 }
 
 
+# function filters data set to be used in 
+# exp_vs_profit_plot function
+exp_vs_profit_filter <- function(df, colname){
+  # df that contains (State, predominate degree, highest degree, Carnegie classification, 
+  #tuition revenue, and instructional expenses)
+  finance_df <- cbind(STABBR = df[,6], df[,14:15], CCBASIC = df[,22], df[,275:276])
+  
+  # getting the mean of all tuition revenue for each classification within each state
+  tuition_rev_df <- finance_df%>%
+    group_by(STABBR, !!sym(colname))%>% # !!sym() = bang bang operator unquotes the variable name
+    summarise(state_tuition_rev_mean = mean(as.numeric(TUITFTE), na.rm = TRUE))
+  
+  # getting the mean of all instructional expenses for each classification within each state
+  expense_df <- finance_df%>%
+    group_by(STABBR, !!sym(colname))%>%
+    summarise(state_expense_mean = mean(as.numeric(INEXPFTE), na.rm = TRUE))
+  
+  # merging the information into one df
+  exp_vs_profit_df  <- cbind(tuition_rev_df, expense_df[,3])
+  
+  # finding the difference between the revenue and expenses
+  exp_vs_profit_df <- exp_vs_profit_df%>%
+    mutate(difference = state_tuition_rev_mean- state_expense_mean)
+  return(exp_vs_profit_df)
+}
+
 df_trimmed<- df_filter(df)
-filtered_df <-df_categorical(df_trimmed)
+filtered_df <-df_shorten(df_trimmed)
 institut_ops <- instit_operations(filtered_df)
 group_plot_filtered_df<-group_bar_filter(filtered_df)
 ridge_plot_df <- ridge_plot_filter(filtered_df)
 
- ######################################################################################
+######################################################################################
+# grouped_bar plot of instructional expenditure per full-time student (INEXPFTE)and net tuition 
+# revenue per full-time student (TUITFTE)
+graphics.off() 
+
+
+# finance_plot <- function(df){
+#   plot <- ggplot(df, aes(x = STABBR, group = PREDDEG))+
+#     geom_bar(aes(y = state_tuition_rev_mean, fill = 'tuition revenue'), stat = 'identity', position = 'dodge' , width = 0.5)+
+#     geom_bar(aes(y = state_expense_mean, fill = 'instructional expense'), stat ='identity', position = 'dodge', width = 0.4)+
+#     scale_fill_manual(values = c('tuition revenue' = 'blue', 'instructional expense' = 'darkorange'))+
+#   labs(
+#       title = 'Mean Tuition Revenue vs Mean Instruct. Expense Per Full-Time Equivalent Student',
+#       x = 'State',
+#       y = 'Mean Value',
+#       fill = 'Category'
+#     )+
+#     theme(axis.title.x = element_text(size = 6, angle = 90))+
+#     theme_classic()
+#   print(plot)
+#   return(plot)
+# }
+
+# create a barplot of the z_core difference of the difference between the mean tuition 
+# revenue and institutional expences per state
+
+z_finance_plot <- function(df, fill, colname){
+  plot <- ggplot(df, aes(x = STABBR, group = !!sym(colname)))+
+    geom_bar(aes(y = !!sym(colname), # z-score value
+                 fill = !!sym(fill)), # coloring the bars based on PREDDEG, HIGHDEG, CCBASIC
+             stat = 'identity', 
+             position = 'dodge', # positions the bars next to each other instead of on top of each other
+             width = 0.5)+
+    scale_fill_viridis(discrete = TRUE, 
+                       option = 'viridis', 
+                       direction = -1)+
+    labs(
+      title = 'Mean Tuition Revenue vs Mean Instruct. Expense Per Full-Time Equivalent Student',
+      x = 'State',
+      y = 'z-score of difference between tuition revenue and instruction expenses',
+      fill = 'Category'
+    )+
+    theme_classic()
+  
+  # plot <- plot + theme(legend.position = 'none')
+  print(plot)
+  return(plot)
+}
+
+# create a multiple of the z_core difference of the difference between the mean tuition 
+# revenue and institutional expenses per state and separating each graph based on state
+z_finance_subplot <- function(df, fill, colname, filename){
+  plots <- lapply(unique(df$STABBR), function(state) {
+    
+    # uses built-in state.abb to group graph based on state 
+    data_subset <- subset(df, STABBR == state) 
+    
+    # calling z_fiance_plot to get each subplot
+    subplot <- z_finance_plot(data_subset, fill, colname)
+    return(subplot)
+  }) # end apply
+  
+  # saving plots into a PDF file
+  pdf(filename)  # Output to a PDF file
+  for (i in 1:length(plots)) {
+    print(plots[[i]])
+  }
+  dev.off()  # Close PDF device
+  
+  return(plots)
+  
+} 
+
+df_trimmed <- df_trimmed%>%
+  filter(CURROPER == 1)
+
+df_trimmed <- state_isolation(df_trimmed)
+
+# sorry for the repeat, I know this could be in a loop- but here we are
+# getting difference based on grouping of preddeg
+preddeg_exp_vs_profit_df <- exp_vs_profit_filter(df_trimmed, 'PREDDEG')
+preddeg_exp_vs_profit_df$zscore <- scale(preddeg_exp_vs_profit_df$difference)[,1] # normalizing calculations using z-score
+
+preddeg_exp_vs_profit_plot <- z_finance_plot(preddeg_exp_vs_profit_df, 'PREDDEG', 'zscore')
+preddeg_exp_vs_profit_subplot <- z_finance_subplot(preddeg_exp_vs_profit_df, 'PREDDEG', 'zscore', 'z_pred_finance_plots.pdf')
+
+
+# getting difference based on grouping of HIGHDEG
+highdeg_exp_vs_profit_df <- exp_vs_profit_filter(df_trimmed, 'HIGHDEG')%>%
+  filter(HIGHDEG != 'Non-degree')
+highdeg_exp_vs_profit_df$zscore <- scale(highdeg_exp_vs_profit_df$difference)[,1] # normalizing calculations using z-score
+
+highdeg_exp_vs_profit_plot <- z_finance_plot(highdeg_exp_vs_profit_df, 'HIGHDEG', 'zscore')
+# highdeg_exp_vs_profit_subplot <- z_finance_subplot(highdeg_exp_vs_profit_df, 'HIGHDEG', 'zscore', 'z_high_finance_plots.pdf')
+
+
+# getting difference based on grouping of CCBASIC
+ccbasic_exp_vs_profit_df <- exp_vs_profit_filter(df_trimmed, 'CCBASIC')
+ccbasic_exp_vs_profit_df$zscore <- scale(ccbasic_exp_vs_profit_df$difference)[,1] # normalizing calculations using z-score
+ccbasic_exp_vs_profit_subplot <- z_finance_subplot(ccbasic_exp_vs_profit_df, 'CCBASIC', 'zscore', 'z_ccbasic_finance_plots.pdf')
+
+## ANALYSIS
+# The bars in the negative means the instructional expenses are greater than the tuition revenue
+# this could indicate that they are wanting to spend more towards delivering educational services.
+# Based on the graphs, it looks like, Associate degree institutions spend more on instructional 
+# expenditures then receiving tuition revenue. 
+
+# Some factors that could play into this
+# 1. a majority of associate programs could be public which means they will more likely be funded by
+# the government
+# 2. the expenses to get an AA is cheaper than a different type of degree so tuition prices may have a
+# lower threshold
+
+# Looking at the HIGHDEG filtering on tuition revenue and instruction expenditure, Certificate programs look to 
+# to have higher revenue in comparison to instruction expenses.
+# 
+# Some factors that could play into this
+# 1. Certificate programs could be online
+# 2. The time frame to complete it is not as long
+# 3. There is not as much upfront cost to get certain materials (for example in a chemistry lab and 
+# the need to get ppe,chemicals, etc.)
+
+
+## Unkwn Variables
+# Does financial aid/ pell grant recipient alter the revenue cost? If so then more people who get these
+# may mean the institution get more federal money from their enrollment.
+######################################################################################
 
 # creating a barplot that looks at the number of institutions in each state 
 bar_plot <- function(df) {
@@ -205,7 +377,7 @@ bar_plot <- function(df) {
 combined_plot <- bar_plot(institut_ops)
 
 ######################################################################################
-# creating a grouped bar plot that looks at the frequency of predominate degree 
+# creating a bar plot that looks at the frequency of predominate degree 
 # within each state and returns a pdf file that contains a graph of each state
 group_bar_plot <- function(df) {
   plots <- lapply(unique(df$STABBR), function(state) {
@@ -250,11 +422,7 @@ group_bar_plot <- function(df) {
 preddeg_plots <- group_bar_plot(group_plot_filtered_df)
 
 ######################################################################################
-# Function to create density plots for different demographic variables 
-# within each PREDDEG group and returns a pdf of all the graphs
-
-######################################################################################
-# Function to create density plots for different demographic variables 
+# Function to create ridge plots for different demographic variables 
 # within each PREDDEG group and returns a pdf of all the graphs
 ridge_plots <- function(df) {
   dem_group <-colnames(df[,5:13]) # getting the column names of the demographics that are listed 
